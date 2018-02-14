09be333c3dfa98b29d174a5039e263675728578b
Ed Perkins <ed@edperkins.co.uk>
2018-02-14 12:47:23 +0000

    update interface.js innerHTML methods

--- src/interface.js
+++ src/interface.js
@@ -1,13 +1,11 @@
-console.log("Hello from interface.js")
-
-function addNote() {
-  var body = document.getElementById('newNoteBox').innerHTML
-  note = new Note(body)
-  loadNote(note)
-}
-
 function loadNote(note) {
   document.getElementById('noteDisplayBox').innerHTML = note.body
+}
+
+function addNote() {
+  var body = document.getElementById('newNoteBox').value
+  note = new Note(body)
+  loadNote(note)
 }
 
 document.getElementById('noteSubmit').onclick = function() {
