b96f2bd59e07552c4869a045240878df5a9d2ea4
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-02-15 12:39:53 +0000

    iterates through an array of test

--- spec/noteListSpec.js
+++ spec/noteListSpec.js
@@ -1,29 +1,37 @@
 (function testNoteStorage() {
 
-  noteList = new NoteList()
+  noteListSpec = []
 
- it("should create a NoteList with an empty array of notes", function() {
+ var test1 = function() {
+   it("should create a NoteList with an empty array of notes", function() {
    return expect.isEmptyArray(noteList.notes)
-  });
+ });
+};
 
-  noteList = new NoteList() //isolates tests
-
-  it("can add a note to NoteList", function() {
+  var test2 = function() {
+    it("can add a note to NoteList", function() {
     var mockNote = {}
     noteList.addNote(mockNote)
     return expect.toInclude(noteList.notes, mockNote)
    });
+ };
 
-   noteList = new NoteList()
-
-   it("can return notes", function() {
+  var test3 = function() { it("can return notes", function() {
      var mockNote1 = {}
      var mockNote2 = {}
      noteList.addNote(mockNote1)
      noteList.addNote(mockNote2)
      return expect.toInclude(noteList.getNotes(), mockNote2)
     });
+  };
 
-
+noteListSpec.push(test1)
+noteListSpec.push(test2)
+noteListSpec.push(test3)
 
 })();
+
+noteListSpec.forEach(test => {
+  noteList = new NoteList()
+  test();
+});
